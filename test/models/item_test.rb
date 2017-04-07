a71efbfe3117af15abb72ae4addc54efea877e0c
Patrick Smith <patrick@burntcaramel.com>
2017-04-07 10:41:12 +1000

    Test Item.kinds

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -22,4 +22,8 @@ class ItemTest < ActiveSupport::TestCase
     item = Item.new price: 3.00, kind: :drink
     assert_not item.valid?
   end
+
+  test "should have kinds" do
+    assert_equal Item.kinds.keys, ['food', 'drink']
+  end
 end
